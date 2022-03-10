c9e085bd98cec1ea2c89e45f3ebf3787f519851f
wu787082055 <84681235+wu787082055@users.noreply.github.com>
2022-03-10 16:02:36 +0800

    嘀嗒音乐业务逻辑

---
+++ dida-server/src/main/java/com/example/yin/constant/Constants.java
@@ -0,0 +1,12 @@
+package com.example.yin.constant;
+
+import org.springframework.beans.factory.annotation.Value;
+
+public class Constants {
+
+    // MAC、Linux系统
+    public static final String RESOURCE_MAC_PATH = "/www/wwwroot/music/client";
+
+    // windos系统
+    public static final String RESOURCE_WIN_PATH = "F:\\24KhsBoy\\music-website-master\\music-website-master\\music-client";
+}
